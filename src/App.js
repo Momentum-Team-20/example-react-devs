055f9eb8053173911562eed4b61ffd8c7104bd15
Amy Gori <amydgori@gmail.com>
2023-11-28 15:14:34 -0500

    Make it about devs, not books

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css'
-import bookData from './book-data.json'
+import devs from './devs.json'
+import { useState } from 'react'
 
 function App() {
   return (
     <div>
-      <h1>Freeshelf</h1>
-      {/* Use bookData to show books on the page*/}
+      <h1>Developers for Hire</h1>
+      <div className="dev-list">
+        {/* loop through the array of devs and make one li for each */}
+        {devs.map((dev) => (
+          <Developer
+            name={dev.name}
+            expertise={dev.expertise}
+            greeting={'Hi hello'}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+function Developer(props) {
+  const [expanded, setExpanded] = useState(false)
+  const handleClick = () => {
+    // expanded = !expanded // this is bad
+    setExpanded(!expanded) // this is good
+  }
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <button onClick={handleClick}>
+        {expanded ? 'show less' : 'show more'}
+      </button>
+      {expanded && <p>{props.expertise}</p>}
     </div>
   )
 }
